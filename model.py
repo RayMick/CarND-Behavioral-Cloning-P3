1505dcb8cb8ebae4fb94d5d3b1e7da3f969badee
RayMick <ruimao20@gmail.com>
2017-10-31 00:27:54 -0700

    check in for 1st review

--- model.py
+++ model.py
@@ -3,13 +3,22 @@ import cv2
 import numpy as np
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
+import os.path
 
 lines = []
+# Read in the training data from driving counter-clock wise
 with open('../../../recordings/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
+print(len(lines))
 
+# Read in the training data from driving clock wise (reverse of counter-clock wise)
+with open('../../../recordings_reverse_track_try2/driving_log.csv') as csvfile2:
+    reader2 = csv.reader(csvfile2)
+    for line2 in reader2:
+        lines.append(line2)
+print(len(lines))
 
 train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
@@ -29,6 +38,12 @@ def generator(samples, batch_size=32):
                 # Need to use \, as windows path are separated by \ instead of /
                 filename = source_path.split('\\')[-1]
                 name = '../../../recordings/IMG/' + filename
+                # If the file is not in dataset dir1, it should be in the dataset dir2
+                if os.path.isfile(name):
+                    pass
+                else:
+                    name = '../../../recordings_reverse_track_try2/IMG/' + filename
+
                 center_image = cv2.imread(name)
                 image_rgb = cv2.cvtColor(center_image, cv2.COLOR_BGR2RGB)
                 # print(image.shape)
@@ -40,7 +55,6 @@ def generator(samples, batch_size=32):
                 angles.append(center_angle * -1.0)
 
 
-            # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
             yield shuffle(X_train, y_train)
@@ -50,6 +64,8 @@ train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
 '''
+# First iteration of the code
+# Reads in all the training data at once
 images = []
 measurements = []
 for i, line in enumerate(lines):
@@ -83,11 +99,12 @@ y_train = np.array(aug_measures)
 
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Activation, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Activation, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 '''
+# Model NO. 1 -- Simplest model without CNN
 model = Sequential()
 model.add(Lambda(lambda x: (x/255.0 - 0.5), input_shape=(160, 320, 3)))
 model.add(Flatten())
@@ -95,6 +112,7 @@ model.add(Dense(1))
 '''
 
 '''
+# Model NO. 2 -- Google Lenet
 model = Sequential()
 model.add(Lambda(lambda x: (x/255.0 - 0.5), input_shape=(160, 320, 3)))
 # Crop the top 60 pixels and bottom 20 pixels
@@ -109,6 +127,8 @@ model.add(Dense(84))
 model.add(Dense(1))
 '''
 
+# Model NO. 3 -- Nvidia CNN
+# This architecture offers the best result for this project
 model = Sequential()
 model.add(Lambda(lambda x: (x/255.0 - 0.5), input_shape=(160, 320, 3)))
 # Crop the top 60 pixels and bottom 20 pixels
@@ -116,12 +136,17 @@ model.add(Cropping2D(cropping=((60, 20), (0, 0))))
 model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
 model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+# Add dropout to prevent overfitting
+model.add(Dropout(0.1))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Dropout(0.1))
 model.add(Dense(50))
+model.add(Dropout(0.1))
 model.add(Dense(10))
+model.add(Dropout(0.1))
 model.add(Dense(1))
 
 
